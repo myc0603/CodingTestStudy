d2c50014db4dc584bbddb76ca56a918cdc624fe0
Yechan Moon <moonyechan97@naver.com>
2025-02-18 18:18:58 +0900

    J9375 test

--- week1/J9375_unsolved.cpp
+++ week1/J9375_unsolved.cpp
@@ -8,21 +8,27 @@ int main() {
     for (int i = 0; i < n; i++) {
         int m;
         cin >> m;
+        cin.ignore();
         if (m == 0) {
             cout << 0;
-            break;
+            continue;
         }
         map<string, int> wearMap;
         for (int j = 0; j < m; j++) {
+            printf("loop: %d/%d\n", j, m);
             string s;
             getline(cin, s);
             int index = s.find(' ');
             string wearKind = s.substr(index + 1);
             wearMap[wearKind]++;
+            cout << "wearKind: " + wearKind + ":"<< wearMap[wearKind] << '\n';
         }
         int ans = 1;
         for (auto it = wearMap.begin(); it != wearMap.end(); it++) {
+            cout << "wearing num: " << it -> second << '\n';
             ans *= (it -> second + 1);
         }
+        ans -= 1;
+        cout << ans;
     }
 }
