f7f6e1e65d9eaf75bf6884cda5b39d6752925df2
Yechan Moon <moonyechan97@naver.com>
2025-02-19 10:38:09 +0900

    fail to solve 1-I

--- week1/I1620_unsolved.cpp
+++ week1/I1620_unsolved.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int n, m;
+    int n, m; // 1 ~ 10만
     cin >> n >> m;
     cout << n << m;
 
@@ -25,8 +25,9 @@ int main() {
     //         cout << mons[index];
     //     }
     // }
-    string monsArr[n+1];
-    map<string, int> monsMap;
+
+    string monsArr[n+1]; // 숫자 나오는 문제 맞추는 용도
+    map<string, int> monsMap; // 포켓몬 이름 나오는 문제 맞추는 용도도
     
     for (int i = 1; i <= n; i++) {
         string s;
@@ -35,6 +36,17 @@ int main() {
         monsArr[i] = s;
     }
 
+    // monsMap, monsArr print
+    cout << "monsMap" << '\n';
+    for (auto it = monsMap.begin(); it != monsMap.end(); it++) {
+        cout << it -> first << " : " << it -> second << '\n';
+    }
+    cout << "monsArr" << '\n';
+    for (int i = 1; i <= n; i++) {
+        cout << monsArr[i] << '\n';
+    }
+    cout << "==========================\n";
+    
     for (int i = 0; i < m; i++) {
         string s;
         cin >> s;
