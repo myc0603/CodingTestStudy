89224617ef658805b62ea7fdb588b61462cfa002
Yechan Moon <moonyechan97@naver.com>
2025-02-19 17:37:13 +0900

    initialize main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,5 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
 
-    if (!(n & 1)) {
-        cout << "it's even\n";
-    } else {
-        cout << "it's odd\n";
-    }
-
-    if (!n & 1) {
-        cout << "it's even\n";
-    } else {
-        cout << "!n: " << !n << '\n';
-        cout << "it's odd\n";
-    }
-
-    if (~n & 1) {
-        cout << "it's even\n";
-    } else {
-        cout << "it's odd\n";
-    }
 }
