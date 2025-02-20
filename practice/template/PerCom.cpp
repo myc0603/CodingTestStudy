8743b28e20079c526055ba1f09a952b555a275de
Yechan Moon <moonyechan97@naver.com>
2025-02-20 22:51:48 +0900

    add template directoroy for practice

---
+++ practice/template/PerCom.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 조합 순열 split() 연습
+
+void printArr() {
+
+}
+
+// 순열 대상인 a의 순서를 바꾸면서 순열을 돈다.
+void makePermutation() {
+
+}
+
+void printVec() {
+
+}
+
+void combiV1() {
+
+}
+
+void combiV2() {
+
+}
+
+int main() {
+    int n = 4;
+    int r1 = 4;
+    int a[] = {1, 2, 3, 4};
+
+    // makePermutation(a, n, r1, 0);
+
+    int r2 = 2;
+    vector<int> result;
+    // combiV1(a, result, n, r2, -1);
+
+    cout << "=============\n";
+
+    result.clear();
+    // combiV2(a, result, n, r2, 0);
+
+    return 0;
+}
