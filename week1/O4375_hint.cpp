2fcd3b4e5d3158b3074b4e68c1ae44611ff00530
Yechan Moon <moonyechan97@naver.com>
2025-02-21 12:28:37 +0900

    1-O

--- week1/O4375_hint.cpp
+++ week1/O4375_hint.cpp
@@ -14,18 +14,21 @@ int main() {
     // n이 한번 들어왔을 때 n : 1 ~ 10,000
     int n;
     cin >> n;
-    if (n == 1) {
-        cout << 1 << '\n';
-        return 0;
-    }
-    int digits = 0;
-    int rem = 1;
-    while(rem) {
-        digits++;
-        rem = (rem + getRem(10, digits, n)) % n;
-        cout << "digits: " << digits << " rem: " << rem << '\n';
+    while (scanf("%d", &n) != EOF) {
+
+        if (n == 1) {
+            cout << 1 << '\n';
+            return 0;
+        }
+        int digits = 0;
+        int rem = 1;
+        while(rem) {
+            digits++;
+            rem = (rem + getRem(10, digits, n)) % n;
+            // cout << "digits: " << digits << " rem: " << rem << '\n';
+        }
+        cout << digits + 1 << '\n';
     }
 
-    cout << digits + 1 << '\n';
     return 0;
 }
