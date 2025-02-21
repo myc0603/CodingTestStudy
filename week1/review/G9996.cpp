9fcc2eab1ddef8f780b6c1d75aa3874734b66334
Yechan Moon <moonyechan97@naver.com>
2025-02-21 11:22:40 +0900

    review 1-G

---
+++ week1/review/G9996.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool match(string pat, string s) {
+    int pos = pat.find('*');
+    string pre = pat.substr(0, pos);
+    string suf = pat.substr(pos + 1);
+
+    if (s.size() < pre.size() + suf.size()) return false;
+    bool cond1 = s.find(pre) == 0;
+    bool cond2 = s.find(suf, s.size() - suf.size());
+    return cond1 && cond2;
+}
+int main() {
+    int n; cin >> n;
+    string pat; cin >> pat;
+    string ans;
+    while (n--) {
+        string s; cin >> s;
+        ans += match(pat, s) ? "DA\n" : "NE\n";
+    }
+    cout << ans;
+}
