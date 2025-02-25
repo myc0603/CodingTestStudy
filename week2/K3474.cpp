c6e6facc9194ef1d1d7f3a0b475827f3b5f48793
Yechan Moon <moonyechan97@naver.com>
2025-02-25 23:39:31 +0900

    solve 2-K

--- week2/K3474.cpp
+++ week2/K3474.cpp
@@ -5,17 +5,18 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
     int t; cin >> t;
-    string result = "";
+    // string result = "";
     while (t--) {
         int n; cin >> n;
         
         int cnt = 0;
         long long d = 5;
-        while (d < n) {
+        while (d <= n) {
             cnt += n / d;
             d *= 5;
         }
-        result += to_string(cnt) + '\n';
+        cout << cnt << '\n';
+        // result += to_string(cnt) + '\n';
     }
-    cout << result;
+    // cout << result;
 }
