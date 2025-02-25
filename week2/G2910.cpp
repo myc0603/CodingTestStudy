1ef30485e9c448697eed402773f4a36cecf56820
Yechan Moon <moonyechan97@naver.com>
2025-02-26 00:45:21 +0900

    solve 2-G

--- week2/G2910.cpp
+++ week2/G2910.cpp
@@ -6,29 +6,33 @@ bool comp(pair<int, int> a, pair<int, int> b) {
 }
 
 int main() {
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL); cout.tie(NULL);
+
     int n, c;
-    cin >> n >> c;
+    cin >> n >> c; // c: 수열 숫자들의 최대 범위...를를 어따쓰지?
     
-    // -> 벡터로 바꿔야 될거 같기도
     // n ~1000 n^2 ~100만 -> 이중 for문 괜찮을듯?
     vector<pair<int, int>> freq; // 숫자, 숫자가 나온 횟수
     for (int i = 0; i < n; i++) {
         int num; cin >> num;
         bool find = false;
         cout << "input num: " << num << '\n';
-        for (auto p : freq) {
+
+        for (auto &p : freq) { // p는 freq안에 있는 pair를 직접 참조하는게 아니라 복사한 값임 p.second++; 같은거 안됨됨
+        // for (int j = 0; j < n; j++) { // out of bounds
             if (p.first == num) {
-                p.second++; // 이거 왜 안되지?
+                p.second++;
                 find = true;
-                cout << "    find, p.first: " << p.first << ", p.second: " << p.second << '\n';
+                cout << "    find, freq[i].first: " << freq[i].first << ", freq[i].second: " << freq[i].second << '\n';
                 break;
             }
         }
         // cout << "find: " << find << '\n';
         if (!find) {
-            freq.push_back({num, 1});
-            cout << "    not found. num: " << num << '\n';
+            freq.push_back({num, 1}); // if 안하면 중복
         }
+        // cout << "    not found. num: " << num << '\n';
     }
 
     for (auto p : freq) {
@@ -42,6 +46,7 @@ int main() {
         while (cnt--) {
             ans += p.first + '0';
             ans += ' ';
+            // cout << p.first << ' ';
         }
     }
     cout << ans << '\n';
