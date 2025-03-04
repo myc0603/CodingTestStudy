8ed64227fafe679c48d764f6181d4a9cbb1c69b9
Yechan Moon <moonyechan97@naver.com>
2025-03-04 21:45:33 +0900

    fail to solve 3-G.. timeover

---
+++ week3/G12851_timeover unsolved.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int start, k;
+    cin >> start >> k;
+
+    int minSec;
+    int cnt = 0;
+    bool flag = false;
+    map<int, int> dis;
+
+    // bfs
+    queue<int> q;
+    q.push(start);
+    dis[start] = 0;
+
+    while (q.size()) {
+
+        int cur = q.front(); q.pop();
+        
+        int nums[] = {cur - 1, cur + 1, cur * 2};
+        for (int num : nums) {
+            if (flag && dis[cur] >= minSec) continue;
+            if (dis.find(num) == dis.end()) {
+                q.push(num);
+                dis[num] = dis[cur] + 1;
+            }
+            if (num == k) {
+                flag = true;
+                minSec = dis[cur] + 1;
+                cnt++;
+            }
+        }
+    }
+
+    cout << minSec << '\n' << cnt << '\n';
+}
