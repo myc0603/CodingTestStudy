0207514cb1f5b77c1dea96f3b2284bfbade573e7
Yechan Moon <moonyechan97@naver.com>
2025-04-03 15:21:25 +0900

    review 2-P

--- week2/review/P14502.cpp
+++ week2/review/P14502.cpp
@@ -12,7 +12,7 @@ bool inMap(int y, int x) {
 
 // 바이러스 전파
 int bfs(int sy, int sx) {
-    int area = 0;
+    int area = -1; // 시작점 제외
 
     queue<pair<int, int>> q;
     q.push({sy, sx});
@@ -26,12 +26,29 @@ int bfs(int sy, int sx) {
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if (inMap(ny, nx) && a[ny][nx] == 0 && virusVisited[ny][nx]) {
+            if (inMap(ny, nx) && a[ny][nx] == 0 && !virusVisited[ny][nx]) {
                 q.push({ny, nx});
                 virusVisited[ny][nx] = 1;
             }
         }
     }
+
+    return area;
+}
+
+int dfs(int y, int x) {
+    int area = 1;
+
+    virusVisited[y][x] = 1;
+
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (inMap(ny, nx) && a[ny][nx] == 0 && !virusVisited[ny][nx]) {
+            area += dfs(ny, nx);
+        }
+    }
+    return area;
 }
 
 int main() {
@@ -48,24 +65,26 @@ int main() {
             }
         }
     }
+    safeArea -= 3; // 벽 3개 설치
 
     int ans = 0;
 
     for (int i = 0; i < blanks.size(); i++) {
         a[blanks[i].first][blanks[i].second] = 1;
 
-        for (int j = 0; j < blanks.size(); j++) {
+        for (int j = i + 1; j < blanks.size(); j++) {
             a[blanks[j].first][blanks[j].second] = 1;
 
-            for (int k = 0; k < blanks.size(); k++) {
+            for (int k = j + 1; k < blanks.size(); k++) {
                 a[blanks[k].first][blanks[k].second] = 1;
 
                 // 바이러스 전파되는 넓이를 구해서 safeArea에 빼서 안전영역 넓이 구한다.
-                int temp = safeArea;
+                int temp = 0;
                 for (auto virus : viruses) {
-                    temp -= bfs(virus.first, virus.second);
+                    // temp += bfs(virus.first, virus.second);
+                    temp += dfs(virus.first, virus.second) - 1;
                 }
-                ans = max(ans, temp);
+                ans = max(ans, safeArea - temp);
 
                 memset(virusVisited, 0, sizeof(virusVisited));
                 a[blanks[k].first][blanks[k].second] = 0;
