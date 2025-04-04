2c7c0637458c825323ba8b7e92779a0b849518af
Yechan Moon <moonyechan97@naver.com>
2025-04-04 18:12:02 +0900

    p2108

--- hidden1/p2108.cpp
+++ hidden1/p2108.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 using namespace std;
 
-int a[500000], cnt[8001]; // 0 ~ 4000, -1 ~ -4000 -> 4001 ~ 8000
+int a[500000], cnt[8001]; // 0 -> 4000, -4000 -> 0 4000 -> 8000
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -12,8 +12,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> a[i];
         sum += a[i];
-        if (a[i] >= 0) cnt[a[i]]++;
-        else cnt[4000-a[i]]++;
+        cnt[4000+a[i]]++;
     }
 
     // 산술평균 -1.8 -> -2
@@ -34,11 +33,14 @@ int main() {
     int mode = -1;
     for (int i = 0; i < 8001; i++) {
         if (mx == cnt[i]) {
+            if (mode != -1) { // 고친 부분
+                mode = i;
+                break;
+            }
             mode = i;
-            if (mode != -1) break;
         }
     }
-    if (mode > 4000) mode = 4000 - mode;
+    mode -= 4000;
     cout << mode << '\n';
 
     // 범위
