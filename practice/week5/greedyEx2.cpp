c9a7a2379e23f021ec31186f9a7c73b58b7c8fe5
Yechan Moon <moonyechan97@naver.com>
2025-04-12 19:46:34 +0900

    greedy ex2

---
+++ practice/week5/greedyEx2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    priority_queue<double> pq;
+    while (n--) {
+        double score; cin >> score;
+    }
+}
