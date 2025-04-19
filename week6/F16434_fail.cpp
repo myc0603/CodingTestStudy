92fc3f2b17d43bb86b5061a3dd9eeb1cac05efec
Yechan Moon <moonyechan97@naver.com>
2025-04-19 23:24:40 +0900

    fail to solve 6-F (HP최댓값 이상으로 회복 못하는 것을 고려 못함)

---
+++ week6/F16434_fail.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+struct Room {
+    int type, attack, hp;
+    Room(int t, int a, int h) : type(t), attack(a), hp(h) {}
+};
+
+int n, atk;
+vector<Room> rooms;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> atk;
+    rooms.reserve(n);
+    long long final_atk = atk;
+    for (int i = 0; i < n; i++) {
+        int t, a, h;
+        cin >> t >> a >> h;
+        rooms.emplace_back(t, a, h);
+        if (t == 2) final_atk += a;
+    }
+
+    long long cur_hp = 1;
+    long long cur_atk = final_atk;
+
+    for (auto room = rooms.rbegin(); room != rooms.rend(); ++room) {
+        cout << "cur HP: " << cur_hp << ", cur ATTACK: " << cur_atk << '\n';
+        if (room -> type == 1) { // monster
+            cout << "  monster attack: " << room -> attack << ", hp: " << room -> hp << '\n';
+            int monster_hp = room -> hp;
+            int monster_atk = room -> attack;
+
+            cout << "    cur hp: " << cur_hp;
+            cur_hp += ((monster_hp + cur_atk - 1) / cur_atk - 1) * monster_atk;
+            cout << " -> " << cur_hp << '\n';
+        } else { // type == 2 potion
+            cout << "  potion attack: " << room -> attack << ", hp: " << room -> hp << '\n';
+
+            cout << "    cur attack: " << cur_atk;
+            cur_atk -= room -> attack;
+            cout << " -> " << cur_atk << "\n";
+
+            cout << "    cur hp: " << cur_hp;
+            cur_hp -= room -> hp;
+            if (cur_hp <= 0) cur_hp = 1;
+            cout << " -> " << cur_hp << '\n';
+        }
+        cout << '\n';
+    }
+
+    cout << cur_hp;
+}
