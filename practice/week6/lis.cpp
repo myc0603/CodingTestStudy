e86cc5c3e0a97d0c81692e1335c08b22e3145444
Yechan Moon <moonyechan97@naver.com>
2025-04-19 11:33:49 +0900

    Longest Increasing Subsequence

--- practice/week6/lis.cpp
+++ practice/week6/lis.cpp
@@ -9,69 +9,134 @@ void printVec(const vector<int> &v) {
 }
 
 int lis_v1(const vector<int> &v) {
-
-    vector<int> dp(v.size(), 1); // 1로 초기화
-    for (int i = 0; i < v.size(); i++) {
+    int n = v.size();
+    vector<int> dp(n, 1); // 1로 초기화
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < i; j++) {
             if (v[j] < v[i]) {
                 dp[i] = max(dp[i], dp[j] + 1);
             }
         }
     }
-    printVec(dp);
+    // printVec(dp);
     return *max_element(dp.begin(), dp.end());
 }
 
 int lis_v2(const vector<int> &v) {
-
+    int n = v.size();
     vector<int> lis;
-    for (int i = 0; i < v.size(); i++) {
+    for (int i = 0; i < n; i++) {
         auto it = lower_bound(lis.begin(), lis.end(), v[i]);
         if (it == lis.end()) lis.push_back(v[i]);
         else *it = v[i];
     }
-    printVec(lis);
+    // printVec(lis);
     return lis.size();
 }
 
-vector<int> get_lis(const vector<int> &v) {
+vector<int> get_lis_gpt(const vector<int> &v) {
     int n = v.size();
     vector<int> lis;
-    vector<int> pos(n);
-    vector<int> prev(n);
-
-    for (int i = 0; i < v.size(); i++) {
+    vector<int> lis_idx;
+    vector<int> prev(n, -1);
+    for (int i = 0; i < n; i++) {
         auto it = lower_bound(lis.begin(), lis.end(), v[i]);
+        int idx = it - lis.begin();
+
         if (it == lis.end()) {
-            // prev는 값 기준이 아닌 인덱스 기준으로 해야 v 안의 요소들의 값이 겹치더라도 문제 되지 않는다.
-            // prev[v[i]] = *lis.rbegin();
             lis.push_back(v[i]);
-            prev[i] = pos[*lis.rbegin()];
+            lis_idx.push_back(i);
         } else {
             *it = v[i];
+            lis_idx[idx] = i;
         }
-        pos[i] = it - lis.begin();
+
+        if (idx > 0) prev[i] = lis_idx[idx-1];
     }
 
     vector<int> real_lis;
-    real_lis.push_back(*lis.rbegin());
-    while (real_lis.size() < lis.size()) {
-        real_lis.push_back(prev[*real_lis.rbegin()]);
+    int cur = lis_idx.back();
+    while (cur != -1) {
+        real_lis.push_back(v[cur]);
+        cur = prev[cur];
     }
     reverse(real_lis.begin(), real_lis.end());
+    
+    for (int num : real_lis) cout << num << ' ';
+    cout << '\n';
+
     return real_lis;
 }
 
+vector<int> get_lis_gpt_with_debug(const vector<int>& v) {
+    int n = v.size();
+    vector<int> lis;                  // 현재까지의 LIS 끝값들
+    vector<int> lis_idx;             // lis[i]의 실제 v[]에서의 인덱스
+    vector<int> prev(n, -1);         // 역추적을 위한 연결 정보
+    vector<int> pos(n);              // v[i]가 LIS의 몇 번째 자리에 들어갔는지
+
+    for (int i = 0; i < n; i++) {
+        auto it = lower_bound(lis.begin(), lis.end(), v[i]);
+        int idx = it - lis.begin(); // v[i]가 들어갈 자리 = 길이 idx+1짜리 LIS
+
+        if (it == lis.end()) {
+            lis.push_back(v[i]);
+            lis_idx.push_back(i);
+        } else {
+            *it = v[i];
+            lis_idx[idx] = i;
+        }
+
+        pos[i] = idx; // v[i]가 길이 idx+1짜리 LIS의 끝이 되었다는 뜻
+        if (idx > 0) {
+            prev[i] = lis_idx[idx - 1]; // 바로 앞 길이의 LIS 마지막 원소 인덱스를 저장
+        }
+
+        // 🔍 디버깅 출력
+        cout << "i = " << i << ", v[i] = " << v[i] << '\n';
+        cout << "  -> position to enter in(idx): " << idx << '\n';
+        cout << "  -> lis: ";
+        for (auto x : lis) cout << x << ' ';
+        cout << "\n  -> lis_idx: ";
+        for (auto x : lis_idx) cout << x << ' ';
+        cout << "\n  -> prev[" << i << "] = " << prev[i];
+        cout << "\n  -> pos[" << i << "] = " << pos[i] << "\n\n";
+    }
+
+    // 역추적
+    vector<int> res;
+    int cur = lis_idx.back();
+    while (cur != -1) {
+        res.push_back(v[cur]);
+        cur = prev[cur];
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+    
 int main() {
     vector<int> a = {10, 20, 10, 30, 20, 50};
     int lis_len1 = lis_v1(a);
     int lis_len2 = lis_v2(a);
+    cout << "length of LIS of vector a: ";
     cout << lis_len1 << ' ' << lis_len2 << '\n';
 
     vector<int> b = {3, 5, 7, 9, 2, 1, 4, 8};
     lis_len1 = lis_v1(b);
     lis_len2 = lis_v2(b);
+    cout << "length of LIS of vector b: ";
     cout << lis_len1 << ' ' << lis_len2 << '\n';
 
+    cout << "LIS of vector a: \n";
+    auto real_lis1 = get_lis_gpt(a);
+    // auto real_lis1 = get_lis_gpt_with_debug(a);
+    for (int num : real_lis1) cout << num << ' ';
+    cout << '\n';
+
+    cout << "LIS of vector b: \n";
+    auto real_lis2 = get_lis_gpt(b);
+    // auto real_lis2 = get_lis_gpt_with_debug(b);
+    for (int num : real_lis2) cout << num << ' ';
+    cout << '\n';
 
 }
