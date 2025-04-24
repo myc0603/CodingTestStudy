80ae90119947c0f91030ef9035b17f3b3286c66b
Yechan Moon <moonyechan97@naver.com>
2025-04-24 13:25:32 +0900

    dp ex2

--- practice/week7/ex2.cpp
+++ practice/week7/ex2.cpp
@@ -6,8 +6,8 @@ int t, n, dp[10001];
 int main() {
     dp[0] = 1;
     for (int i = 1; i <= 3; i++) {
-        for (int j = 1; j <= 10000; j++) {
-            if (j - i >= 0) dp[j] += dp[j - i];
+        for (int j = i; j <= 10000; j++) {
+            dp[j] += dp[j - i];
         }
     }
 
