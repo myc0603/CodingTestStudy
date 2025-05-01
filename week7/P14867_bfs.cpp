2ffb566abef6d9a2af6f31069e4ca8e9fe4b644c
Yechan Moon <moonyechan97@naver.com>
2025-05-01 09:33:56 +0900

    bfs - 34점

---
+++ week7/P14867_bfs.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int capA, capB, endA, endB;
+int visited[1002][1002];
+
+vector<pair<int, int>> nextAmounts(int curA, int curB) {
+    vector<pair<int, int>> v;
+    if (curA != 0) v.push_back( {0, curB} );
+    if (curB != 0) v.push_back( {curA, 0} );
+    if (curA != capA) v.push_back( {capA, curB} );
+    if (curB != capB) v.push_back( {curA, capB} );
+    if (curA != 0 && curB != capB) {
+        if (curA > capB - curB) v.push_back( {curA - (capB - curB), capB} );
+        else v.push_back( {0, curB + curA} );
+    }
+    if (curB != 0 && curA != capA) {
+        if (curB > capA - curA) v.push_back( {capA, curB - (capA - curA)} );
+        else v.push_back( {curA + curB, 0} );
+    }
+    return v;
+}
+
+int bfs() {
+    queue<pair<int, int>> q;
+    q.push( {0, 0} );
+    visited[0][0] = 1;
+
+    while (q.size()) {
+        int a = q.front().first;
+        int b = q.front().second;
+        q.pop();
+
+        if (a == endA && b == endB) {
+            return visited[endA][endB] - 1;
+        }
+
+        auto nextABs = nextAmounts(a, b);
+        for (auto p : nextABs) {
+            if (visited[p.first][p.second]) continue;
+            q.push(p);
+            visited[p.first][p.second] = visited[a][b] + 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    cin >> capA >> capB >> endA >> endB;
+    cout << bfs();
+}
