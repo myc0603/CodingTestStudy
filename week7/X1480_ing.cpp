6daeff45766e0ccbb9952d1ac95f482a0a0e268e
Yechan Moon <moonyechan97@naver.com>
2025-05-05 18:32:38 +0900

    ing

--- week7/X1480_ing.cpp
+++ week7/X1480_ing.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <cstring>
 using namespace std;
 
-int n, m, c;
-vector<int> w, dp[22];
+int n, m, c, w[15], dp[12][22];
+
+int dfs(int bag, int curWeight) {
+
+    if (bag == m) return 1;
+    
+    int &result = dp[bag][curWeight];
+    if (result != -1) return result;
+
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        if (curWeight + w[i] <= c) {
+            cnt += dfs(bag, curWeight + w[i]);
+        } else {
+            cnt += dfs(bag + 1, w[i]);
+        }
+    }
+    return cnt;
+}
 
 int main() {
     cin >> n >> m >> c;
-    w.resize(n);
     for (int i = 0; i < n; ++i) cin >> w[i];
 
-    int cnt = 0;
-    // m으로도 루프 돌아야 됨
-    while (m--) {
-        for (int weight : w) {
-            for (int j = c; j >= weight; --j) {
-                if (dp[j].size() < dp[j-weight].size() + 1) {
-                    dp[j] = dp[j-weight];
-                    dp[j].push_back(weight);
-                }
-            }
-        }
-
-        cnt += dp[c].size();
-        for (int jewel : dp[c]) w.erase(find(w.begin(), w.end(), jewel));
-        for (auto &v : dp) v.clear();
-    }
-    cout << cnt;
+    memset(dp, -1, sizeof(dp));
+    cout << dfs(0, 0);
 }
