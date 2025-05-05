319ce0ea5116b1941f7470fcd62221954823596d
Yechan Moon <moonyechan97@naver.com>
2025-05-05 17:56:08 +0900

    s-sum 조건 추가

--- stepbystep/MeetInTheMiddle/1208_countingmap.cpp
+++ stepbystep/MeetInTheMiddle/1208_countingmap.cpp
@@ -2,7 +2,7 @@
 // #include <unordered_map>
 using namespace std;
 
-const int BIAS = 3000000; // s - sum 절댓값의 범위가 최대 300만
+const int BIAS = 2000000; // s - sum 절댓값의 범위가 최대 300만
 int n, s, a[41] , countingmap[2*BIAS+1];
 // unordered_map<int, long long> countingmap;
 long long cnt;
@@ -18,7 +18,9 @@ void dfs1(int cur, int sum) {
 
 void dfs2(int cur, int sum) {
     if (cur == n) {
-        cnt += countingmap[BIAS + (s - sum)];
+        if (abs(s - sum) <= BIAS) { // 이 조건을 위와 합치면 안 됨 return은 같이 안 묶이기 때문에
+            cnt += countingmap[BIAS + (s - sum)];
+        }
         return;
     }
     dfs2(cur + 1, sum);
