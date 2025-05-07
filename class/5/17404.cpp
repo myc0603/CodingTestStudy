892e70bd77b145e19c9c298267e77cd253e02237
Yechan Moon <moonyechan97@naver.com>
2025-05-07 21:02:54 +0900

    clean

--- class/5/17404.cpp
+++ class/5/17404.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int n, cost[1000][3], dp[3][1000][3];
+int n, cost[1000][3], dp[3][1002][3];
 
 int dfs(int firstColor, int idx, int prevColor) {
     int &result = dp[firstColor][idx][prevColor];
     if (result) return result;
 
+    result = 1e9;
     if (idx == n - 1) {
-        int cand = (firstColor + 1) % 3;
-        if (cand == prevColor) {
-            result = cost[n-1][(cand + 1) % 3];
-        } else {
-            result = cost[n-1][cand];
-            if ((cand + 1) % 3 != prevColor) result = min(result, cost[n-1][(cand + 1) % 3]);
+        for (int color = 0; color < 3; ++color) {
+            if (color == firstColor || color == prevColor) continue;
+            result = min(result, cost[n-1][color]);
         }
         return result;
     }
 
-    int curColor1 = (prevColor + 1) % 3;
-    int curColor2 = (prevColor + 2) % 3;
-    result = min(dfs(firstColor, idx + 1, curColor1) + cost[idx][curColor1],
-                    dfs(firstColor, idx + 1, curColor2) + cost[idx][curColor2]);
+    for (int c = 0; c < 3; ++c) {
+        if (c == prevColor) continue;
+        result = min(result, dfs(firstColor, idx + 1, c) + cost[idx][c]);
+    }
     return result;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     cin >> n;
     for (int i = 0; i < n; ++i) {
         cin >> cost[i][0] >> cost[i][1] >> cost[i][2];
