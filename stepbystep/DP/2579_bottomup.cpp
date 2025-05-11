37cdf46cd70e3d012345dc0f7389a3a9655e97d4
Yechan Moon <moonyechan97@naver.com>
2025-05-11 21:50:44 +0900

    solve - bottom up dp

---
+++ stepbystep/DP/2579_bottomup.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int n, scores[303], dp[303][4];
+
+// 마지막 도착 계단은 반드시 밟아야 한다.
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; ++i) cin >> scores[i];
+
+    dp[1][1] = scores[1];
+
+    for (int i = 1; i <= n; ++i) {
+        if (i >= 2) {
+            dp[i][1] = max(dp[i-2][1], dp[i-2][2]) + scores[i];
+        }
+        dp[i][2] = dp[i-1][1] + scores[i];
+    }
+
+    cout << max(dp[n][1], dp[n][2]);
+}
