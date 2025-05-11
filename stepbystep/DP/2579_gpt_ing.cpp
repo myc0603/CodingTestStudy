fdaa4172b59e9cd562e1691850d952227ef4fbd7
Yechan Moon <moonyechan97@naver.com>
2025-05-11 22:02:05 +0900

    ing

---
+++ stepbystep/DP/2579_gpt_ing.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int n, scores[303], dp[303][4];
+
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; ++i) cin >> scores[i];
+
+}
---
+++ week7/V14863_bottomup_ing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int n, k, wt[101], wv[101], bt[101], bv[101], dp[101][100003];
+
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; ++i) {
+        cin >> wt[i] >> wv[i] >> bt[i] >> bv[i];
+    }
+
+    
+}
