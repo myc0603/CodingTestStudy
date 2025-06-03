437fe5aaa893e0bc8bf555fd2e53f1e13701eb88
Yechan Moon <moonyechan97@naver.com>
2025-06-03 11:06:38 +0900

    ing

--- class/3/p14500_DP.cpp
+++ class/3/p14500_DP.cpp
@@ -2,11 +2,45 @@
 #include <queue>
 using namespace std;
 
-// DP로 풀기~~!!
+int n, m, dp[5][510][510], dp2[5][510][510], ans;
 
-int n, m, a[502][502], dp[502][502][5];
-int dy[] = {1, 0, 0};
-int dx[] = {0, 1, -1};
+int main() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            cin >> dp[1][i][j];
+        }
+    }
 
-// dp[i][j][4]
-// dp[i+1][j][2]
+    // dp[2][i][j];
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            dp[2][i][j] = dp[1][i][j] + max(dp[1][i][j+1], dp[1][i+1][j]);
+            dp2[2][i][j] = dp[1][i][j] + max(dp[1][i][j+1], dp[1][i-1][j]);
+        }
+    }
+
+    // dp[3][i][j]
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            dp[3][i][j] = dp[1][i][j] + max(dp[1][i][j+1] + dp[1][i+1][j], max(dp[2][i][j+1], dp[2][i+1][j]));
+            dp2[3][i][j] = dp[1][i][j] + max(dp2[2][i][j+1], dp2[2][i-1][j]);
+        }
+    }
+
+    // dp[4][i][j]
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            int temp = max(max(dp[3][i][j+1], dp[3][i+1][j]), max(dp[2][i][j+1] + dp[1][i+1][j], dp[1][i][j+1] + dp[2][j+1][i]));
+            temp = max(temp, max(dp2[3][i][j+1], dp2[3][i-1][j]));
+            temp = max(temp, max(dp[1][i][j+1] + dp[1][i][j+1] + dp[1][i-1][j+1], dp[1][i+1][j] + dp[1][i+2][j] + dp[1][i+1][j-1])); // ㅗ, ㅓ
+            // dp[4][i][j] = dp[1][i][j] + temp;
+
+            ans = max(ans, dp[1][i][j] + temp);
+        }
+    }
+
+    cout << ans;
+
+    
+}
