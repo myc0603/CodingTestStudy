7a0425b70a5f665931b39c5bb2c773bd9e7552be
Yechan Moon <moonyechan97@naver.com>
2025-06-07 21:40:36 +0900

    ing

--- class/4/silver/1991_ing.cpp
+++ class/4/silver/1991_ing.cpp
@@ -2,11 +2,42 @@
 #include <vector>
 using namespace std;
 
-const char BASE_CHAR = 'A';
-int n;
+const char BASE_CHAR = 'A' - 1;
+int n, visited[30];
 vector<int> adj[30];
 
-void dfs();
+void preorder(int cur) {
+    visited[cur];
+    cout << BASE_CHAR + (char) cur;
+
+    int left = adj[cur][0];
+    if (left) preorder(left);
+
+    int right = adj[cur][1];
+    if (right) preorder(right);
+}
+
+void inorder(int cur) {
+    int left = adj[cur][0];
+    if (left) preorder(left);
+    
+    visited[cur];
+    cout << BASE_CHAR + (char) cur;
+
+    int right = adj[cur][1];
+    if (right) preorder(right);
+}
+
+void postorder(int cur) {
+    int left = adj[cur][0];
+    if (left) preorder(left);
+
+    int right = adj[cur][1];
+    if (right) preorder(right);
+
+    visited[cur];
+    cout << BASE_CHAR + (char) cur;
+}
 
 int main() {
     cin >> n;
@@ -16,7 +47,17 @@ int main() {
     for (int i = 0; i < n; ++i) {
         char me, left, right;
         cin >> me >> left >> right;
-        adj[me-BASE_CHAR][0] = left;
-        adj[me-BASE_CHAR][1] = right;
+        if (left != '.') adj[me-BASE_CHAR][0] = left;
+        if (right != '.') adj[me-BASE_CHAR][1] = right;
     }
+
+    preorder(1);
+    cout << '\n';
+
+    fill(visited, visited + n, 0);
+    inorder(1);
+    cout << '\n';
+
+    fill(visited, visited + n, 0);
+    postorder(1);
 }
