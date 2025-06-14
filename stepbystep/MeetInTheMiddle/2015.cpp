3e096f0cde3cebfcc45c5f2387a25d2b12b1c7c5
Yechan Moon <moonyechan97@naver.com>
2025-06-14 10:49:38 +0900

    solve 2015

--- stepbystep/MeetInTheMiddle/2015.cpp
+++ stepbystep/MeetInTheMiddle/2015.cpp
@@ -3,20 +3,21 @@
 using namespace std;
 typedef long long ll;
 
-int n, k, a[200003], psum[200003];
+int n, k, a, psum[200003];
 unordered_map<ll, ll> counts;
 
 int main() {
-    cin >> n >> k;
-    for (int i = 1; i <= n; ++i) {
-        cin >> a[i];
-        psum[i] = psum[i-1] + a[i];
-        ++counts[psum[i] + k];
-    }
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     ll cnt = 0;
+    cin >> n >> k;
+    ++counts[k]; // psum[0]+k == k
     for (int i = 1; i <= n; ++i) {
+        cin >> a;
+        psum[i] = psum[i-1] + a;
         cnt += counts[psum[i]];
+        ++counts[psum[i] + k];
     }
     cout << cnt;
 }
